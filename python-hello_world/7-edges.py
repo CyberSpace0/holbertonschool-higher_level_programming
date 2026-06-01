9c9f14837f36844b6c5ef70ed005bcbeeed0e371
MN7 <dutymastr@outlook.com>
2026-06-01 14:53:37 +0300

    Holberton Task

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -2,5 +2,5 @@
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"First 3 letters: {word[0:3]}")
-print(f"Last 2 letters: {word[7:9]}")
+print(f"Last 2 letters: {word[-2:25]}")
 print(f"Middle word: {word[1:-1]}")
