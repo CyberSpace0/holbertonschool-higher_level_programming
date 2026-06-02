3ef2b29f9013218d06ac7757fc3afa98f3eab17d
MN7 <dutymastr@outlook.com>
2026-06-02 14:34:25 +0300

    Holberton Task

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ if (number < 0):
 else:
     digit = f"{str(number)[-1]}"
 
-if(int(digit) > 5):
+if (int(digit) > 5):
     r = "and is greater than 5"
-elif(int(digit) == 0):
+elif (int(digit) == 0):
     r = "and is 0"
 else:
     r = "and is less than 6 and not 0"
