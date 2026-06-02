1ac8b360bd5e94a08985fc657eeee8d08d79f85d
MN7 <dutymastr@outlook.com>
2026-06-02 13:25:37 +0300

    Holberton Task

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-
-print("object-oriented programming with Python")
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:66]
+print(f"{str} with Python")
