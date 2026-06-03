eeb18a8a10e7f3e5825cc8408adfa427c2b95c75
MN7 <dutymastr@outlook.com>
2026-06-03 11:28:09 +0300

    Holberton Task

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,15 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    a = [3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60, 63, 66, 69, 72, 75, 78, 81, 84, 87, 90, 93, 96, 99]
-    b = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100]
-
     for i in range(1, 101):
-        if (i in a and i in b):
-            print("FizzBuzz ", end="")
-        elif (i in a):
-            print("Fizz ", end="")
-        elif (i in b):
-            print("Buzz ", end="")
+        if i % 15 == 0:
+            print("FizzBuzz", end=" ")
+        elif i % 3 == 0:
+            print("Fizz", end=" ")
+        elif i % 5 == 0:
+            print("Buzz", end=" ")
         else:
-            print(f"{i} ", end="")
-
+            print(i, end=" ")
