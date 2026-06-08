3b764eb80a2e3e2ccd8b504d47ba5387fd09cc43
MN7 <dutymastr@outlook.com>
2026-06-08 10:54:18 +0300

    Holberton Task

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    new = ""
     for i in my_string:
         if (i != 'c' and i != 'C'):
-            print("{:d}".format(i),end="")
+            new.join(i)
+    return new
