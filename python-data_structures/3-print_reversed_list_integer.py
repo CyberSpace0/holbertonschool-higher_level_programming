38920f2875d7520f03c8f41fae1b267b39705f24
MN7 <dutymastr@outlook.com>
2026-06-08 10:25:58 +0300

    Holberton Task

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    a = len(my_list) - 1
-    x = 0
+    a = len(my_list)
+    x = 1
     for i in range(a):
         print("{:d}".format(my_list[a - x]))
         x = x - 1
