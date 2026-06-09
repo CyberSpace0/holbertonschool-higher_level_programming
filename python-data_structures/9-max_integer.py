37295a5cd299273fe47c11e805d49c09d5d87b85
MN7 <dutymastr@outlook.com>
2026-06-09 12:18:06 +0300

    Holberton Task

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if (len(my_list) <= 0):
+    if len(my_list) == 0:
         return None
-    a = True
+
+    maximum = my_list[0]
+
     for i in my_list:
-        for x in my_list:
-            if (i > x):
-                a = True
-            else:
-                a = False
-        if (a == True):
-            return i
-            break
+        if i > maximum:
+            maximum = i
+
+    return maximum
