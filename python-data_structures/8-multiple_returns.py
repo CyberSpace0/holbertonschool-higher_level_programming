8cea838be6ee2a014e98d56ffd05e2e83e368e5f
MN7 <dutymastr@outlook.com>
2026-06-09 11:49:43 +0300

    Holberton Task

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     try:
-        return len(sentence),sentence[0]
-    except:
-        return 0,None
+        return len(sentence), sentence[0]
+    except Exception:
+        return 0, None
