db250d0b13aef3eddc0e1cd3d2c68e5c0731e946
MN7 <dutymastr@outlook.com>
2026-06-09 13:06:02 +0300

    Holberton Task

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     new = my_list
-    if idx < 0 or idx >= len(my_list):
-        return new
-
-    return new[:idx] + new[idx + 1:]
+    del new[idx]
+    return new
