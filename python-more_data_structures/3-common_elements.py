35aac18dd070126f64108065714870f3a8f4ae74
MN7 <dutymastr@outlook.com>
2026-06-10 10:50:00 +0300

    Holberton Task

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    x = []
     for i in set_2:
         if (i in set_1):
-            return i
-    return []
+            x.append(i)
+    return x
