6d1acd5d83a6d565056c8b110388c2dc1bb572c0
MN7 <dutymastr@outlook.com>
2026-06-10 11:35:49 +0300

    Holberton Task

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    new = []
-    dub = list(set(set_1) & set(set_2))
-    for i in set_1:
-        if (list(i) != dub):
-            new.append(i)
-    for i in set_2:
-        if (list(i) != dub):
-            new.append(i)
-    return new
+    return set_1 ^ set_2
