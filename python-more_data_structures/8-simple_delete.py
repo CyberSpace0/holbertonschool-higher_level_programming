838af4911993aabebbbe14f167ba6ed6fc1be175
MN7 <dutymastr@outlook.com>
2026-06-11 12:04:41 +0300

    Holberton Task

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    if (key == ""):
-        return a_dictionary
-    else:
-        dict.pop(a_dictionary,key)
+    try:
+        if (key == "" ):
+            return a_dictionary
+        else:
+            dict.pop(a_dictionary,key)
+            return a_dictionary
+    except:
         return a_dictionary
