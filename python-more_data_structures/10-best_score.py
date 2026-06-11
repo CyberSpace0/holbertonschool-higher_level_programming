a270953c9ec747e618168ca2b03071b4163af138
MN7 <dutymastr@outlook.com>
2026-06-11 12:41:47 +0300

    Holberton Task

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if (a_dictionary == None or len(a_dictionary) <= 0):
+    if (not a_dictionary or len(a_dictionary) <= 0):
         return None
     try:
         a_dictionary["John"]
