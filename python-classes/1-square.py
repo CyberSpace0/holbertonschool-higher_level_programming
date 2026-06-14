1e426858a633ecf9c8650f67910af6158057c779
MN7 <dutymastr@outlook.com>
2026-06-14 12:53:59 +0300

    Holberton Task

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module is empty """
+
+
+class Square:
+    """class has private attribuate called size"""
+    def __init__(self,size):
+        """ class has specifc variable """
+        self.__size = size
