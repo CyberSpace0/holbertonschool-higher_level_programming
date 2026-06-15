b33c2e031c004b57420cb23c431077c0987e21fd
MN7 <dutymastr@outlook.com>
2026-06-15 13:55:57 +0300

    Holberton Task

---
+++ python-classes/3-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """This class represents a square."""
+
+    def __init__(self, size=0):
+        """Initialize a square with a validated size."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+    
+    def area(self):
+        """ returns the current square area """
+        return (self.__size * self.__size)
