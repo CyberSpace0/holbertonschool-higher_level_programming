a0e90252ca370dc330da37ccb367862a5f912046
MN7 <dutymastr@outlook.com>
2026-06-16 12:12:48 +0300

    Holberton Task

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,19 +6,25 @@ class Square:
     """This class represents a square."""
 
     def __init__(self, size=0):
-        """Initialize a square with a validated size."""
-        def size(self, value): 
-            if type(value) is not int:
-                raise TypeError("size must be an integer")
+        """Initialize a square with a size."""
+        self.size = size
 
-            if value < 0:
-                raise ValueError("size must be >= 0")
+    @property
+    def size(self):
+        """Retrieve the size of the square."""
+        return self.__size
 
-            self.__size = value
-            return self.__size
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = value
 
     def area(self):
-        """returns the current square area"""
+        """Return the current square area."""
         return self.__size * self.__size
-
-print(Square().area())
