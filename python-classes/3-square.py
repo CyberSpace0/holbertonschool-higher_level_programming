e167b2ed575b0a0f382445cd0958a13ffa8f3099
MN7 <dutymastr@outlook.com>
2026-06-16 11:22:27 +0300

    Holberton Task

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,6 +14,7 @@ class Square:
             raise ValueError("size must be >= 0")
 
         self.__size = size
+
     def area(self):
         """returns the current square area"""
         return self.__size * self.__size
