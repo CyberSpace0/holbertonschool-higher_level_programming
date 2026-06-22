b1c1eb0565570a58714c11ef65e6cdae0a478292
MN7 <dutymastr@outlook.com>
2026-06-22 13:36:39 +0300

    Holberton Task

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Module containing lookup function."""
+
 
 def lookup(obj):
-    print(dir(obj))
+    """Return the list of available attributes and methods."""
+    return dir(obj)
