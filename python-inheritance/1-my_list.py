96d1b69f8ab8e60f0c6fb81dc572bf1b529357a2
Your Name <you@example.com>
2026-06-24 21:51:19 +0300

    Holberton task

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module containing lookup function."""
+
+class MyList(list):
+    """list class"""
+    def print_sorted(self):
+        """Return the list of available attributes and methods."""
+        print(sorted(self))
