f529b8dad0c5dfe1109c1244a5eddc5e1f23b88f
MN7 <dutymastr@outlook.com>
2026-06-25 09:28:38 +0300

    Holberton Task

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -12,5 +12,5 @@ class BaseGeometry:
         """integer validator"""
         if (type(value) is not int):
             raise TypeError(f"{name} must be an integer")
-        elif(value <= 0):
+        elif (value <= 0):
             raise ValueError(f"{name} must be greater than 0")
