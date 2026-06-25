158e919fb1845f18acdc8e40c4d355e8edeb96aa
MN7 <dutymastr@outlook.com>
2026-06-25 10:51:38 +0300

    Holberton Task

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module containing Rectangle class."""
+
+
+class Animal:
+    """Animal"""
+    def sound(self):
+        """sound function"""
+        pass
+    
+class Dog(Animal):
+    """dog"""
+    def sound(self):
+        """sound function"""
+        return "Bark"
+
+class Cat(Animal):
+    """cat"""
+    def sound(self):
+        """sound function"""
+        return "Meow"
