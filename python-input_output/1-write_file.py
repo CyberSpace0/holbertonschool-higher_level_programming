8ca0d71e224760bb7ae24d60fc0ace2c3fd442fd
MN7 <dutymastr@outlook.com>
2026-06-28 14:40:57 +0300

    Holberton Task

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,4 +5,4 @@
 def write_file(filename="", text=""):
     """Read a UTF-8 text file."""
     with open(filename, "a", encoding="utf-8") as file:
-        file.write(text)
+        print(file.write(text))
