b7905f002ce4b5338e739f3764067564b75d209c
MN7 <dutymastr@outlook.com>
2026-06-28 14:38:17 +0300

    Holberton Task

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module containing read_file function."""
+
+
+def read_file(filename=""):
+    """Read a UTF-8 text file."""
+    with open(filename, "a", encoding="utf-8") as file:
+        print(file.read(), end="")
