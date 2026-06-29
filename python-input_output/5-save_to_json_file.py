c2347279472619232234c4dae7ee29dbd01b8065
MN7 <dutymastr@outlook.com>
2026-06-29 13:51:25 +0300

    Holberton Task

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module containing read_file function."""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Json to Object"""
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj))
