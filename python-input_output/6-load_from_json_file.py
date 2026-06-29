b3923d0fcca54fb1ab7308a2823e8d76c12b2298
MN7 <dutymastr@outlook.com>
2026-06-29 13:58:29 +0300

    Holberton Task

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module containing read_file function."""
+import json
+
+
+def load_from_json_file(filename):
+    """Json to Object"""
+    with open(filename, "r", encoding="utf-8") as f:  
+        return json.load(f.read(filename))
