9d77e5d5296cdf42f1ac79216179bc28cc8df126
MN7 <dutymastr@outlook.com>
2026-06-30 13:49:12 +0300

    Holberton Task

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,11 +5,11 @@ import pickle
 
 def serialize_and_save_to_file(data, filename):
     """serialize"""
-    with open(filename,"wb") as f:
+    with open(filename,"wb", encoding='utf-8') as f:
         pickle.dump(data, f)
 
 def load_and_deserialize(filename):
     """desirialize"""
-    with open(filename, "rb") as rd:
+    with open(filename, "rb", encoding='utf-8') as rd:
         load = pickle.load(rd)
     return load
