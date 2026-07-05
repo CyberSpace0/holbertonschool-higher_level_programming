f514bc487d19cfefef7ca9f98f69404b4389cdcd
MN7 <dutymastr@outlook.com>
2026-07-05 13:57:52 +0300

    Holberton Task

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -54,16 +54,9 @@ def add_user():
     if username in users:
         return jsonify({"error": "Username already exists"}), 409
 
-    users[username] = {
-        "name": data.get("name"),
-        "age": data.get("age"),
-        "city": data.get("city")
-    }
+    users[username] = data
 
-    return jsonify({
-        "message": "User added",
-        "user": users[username]
-    }), 201
+    return jsonify(data), 201
 
 
 if __name__ == "__main__":
