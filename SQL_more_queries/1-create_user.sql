523ac4680fe980a0534798861f405c8632b44002
MN7 <dutymastr@outlook.com>
2026-07-21 11:02:07 +0300

    Holberton Task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- create user
+SET GLOBAL validate_password.policy=LOW; SET GLOBAL validate_password.length=8;
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON mysql.* TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
