947e5cd3d0479db0a69514b1f5bad82c9b60b259
MN7 <dutymastr@outlook.com>
2026-07-21 11:03:35 +0300

    Holberton Task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,3 @@
--- create user
-SET GLOBAL validate_password.policy=LOW; SET GLOBAL validate_password.length=8;
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
-
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+-- creates the MySQL server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL ON *.* TO 'user_0d_1'@'localhost';
