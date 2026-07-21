b072005a0c36a965fe418842aadd8c0587683b0c
MN7 <dutymastr@outlook.com>
2026-07-21 11:02:56 +0300

    Holberton Task

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,6 @@
 -- create user
 SET GLOBAL validate_password.policy=LOW; SET GLOBAL validate_password.length=8;
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
