b98ad6871d0279f60da11520cc46cebb1ad4f692
MN7 <dutymastr@outlook.com>
2026-07-22 11:45:47 +0300

    Holberton Task

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     )
 
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM STATES WHERE NAME={sys.argv[4]}")
+    cur.execute(f"SELECT * FROM states WHERE name='{sys.argv[4]}'")
     rows = cur.fetchall()
     for row in rows:
         print(row)
